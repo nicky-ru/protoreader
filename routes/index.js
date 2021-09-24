d9db3d0ff1c90193990b61bbf209ff2109f767e4
Nicky Ru <nikitkaruban@gmail.com>
2021-09-24 23:33:45 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Client = require('../apollo/client');
-const queries = require('../graphql/queries');
-const gql = require('graphql-tag');
-const decoder = require('../lib/decoder');
+// const Client = require('../apollo/client');
+// const queries = require('../graphql/queries');
+// const gql = require('graphql-tag');
+// const decoder = require('../lib/decoder');
 
 // res.set('Access-Control-Allow-Origin', '*');
 
@@ -12,21 +12,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.get('/api/devices', async function (req, res, next) {
-//   console.log("querying devices");
-//   const queryResult = await Client.query({query: gql(queries.getDevices)});
-//   res.json(queryResult.data.devices);
-// });
-//
-// router.get('/api/devices/:imei', async function (req, res, next) {
-//   console.log("querying device records");
-//   const imei = req.params.imei;
-//   const queryResult = await Client.query({
-//     query: gql(queries.deviceRecords),
-//     variables: {imei: imei}
-//   })
-//   const decodedTelemetry = decoder.decodeTelemetry(queryResult.data.deviceRecords);
-//   res.json(decodedTelemetry);
-// })
+router.get('/api/devices', async function (req, res, next) {
+  console.log("querying devices");
+  // const queryResult = await Client.query({query: gql(queries.getDevices)});
+  // res.json(queryResult.data.devices);
+  res.json({"data":"data"})
+});
+
+router.get('/api/devices/:imei', async function (req, res, next) {
+  console.log("querying device records");
+  // const imei = req.params.imei;
+  // const queryResult = await Client.query({
+  //   query: gql(queries.deviceRecords),
+  //   variables: {imei: imei}
+  // })
+  // const decodedTelemetry = decoder.decodeTelemetry(queryResult.data.deviceRecords);
+  // res.json(decodedTelemetry);
+  res.json({"data":"data"});
+})
 
 module.exports = router;
