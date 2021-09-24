0aa891b25cc029c4f4500e73d4d8ee4c02ef0f01
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 02:10:07 +0800

    add fetch device Records

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,22 @@ const express = require('express');
 const Client = require('../middleware/client');
 const queries = require('../middleware/graphql/queries');
 const getDevices = queries.getDevices;
+const deviceRecords = queries.deviceRecords;
 const gql = require('graphql-tag');
+const decoder = require('../middleware/decoder');
+
+// res.set('Access-Control-Allow-Origin', '*');
 
 exports.devices = async (req, res, next) => {
     console.log("querying devices");
     const queryResult = await Client.query({query: gql(getDevices)});
     res.json(queryResult.data.devices);
+}
+
+exports.deviceRecords = async (req, res, next) => {
+    console.log("querying device records");
+    const imei = req.params.imei;
+    const queryResult = await Client.query({query: gql(deviceRecords), variables: {imei: imei}});
+    const decodedTelemetry = decoder.decodeTelemetry(queryResult.data.deviceRecords);
+    res.json(decodedTelemetry);
 }
