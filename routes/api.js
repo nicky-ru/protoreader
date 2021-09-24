bd2a6200c9f3463b975b7e529a50f59e1ea1c545
Nicky Ru <nikitkaruban@gmail.com>
2021-09-25 02:00:42 +0800

    add devices fetching

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 'use strict';
 const express = require('express');
+const Client = require('../middleware/client');
+const queries = require('../middleware/graphql/queries');
+const getDevices = queries.getDevices;
+const gql = require('graphql-tag');
 
-exports.devices = (req, res, next) => {
-    res.json({"devicdes": "devices"})
+exports.devices = async (req, res, next) => {
+    console.log("querying devices");
+    const queryResult = await Client.query({query: gql(getDevices)});
+    res.json(queryResult.data.devices);
 }
