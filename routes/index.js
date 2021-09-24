27a89079f72d65c1db2932073da8743724b804af
Nicky Ru <nikitkaruban@gmail.com>
2021-09-24 22:45:50 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const SensorData = require('./pebbleproto');
+const Client = require('../apollo/client');
+const queries = require('../graphql/queries');
+const gql = require('graphql-tag');
+const decoder = require('../lib/decoder');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/telemetry', function (req, res, next) {
-  res.set('Access-Control-Allow-Origin', '*');
-  const raw = req.body.raw;
-  const encodedTelemetry = raw.replace(/0x/g, '');
-  console.log(encodedTelemetry);
-  const telemetry = SensorData.decode(Buffer.from(encodedTelemetry, 'hex'));
-  res.json(telemetry);
+router.get('/api/devices', async function (req, res, next) {
+  console.log("querying devices");
+  const queryResult = await Client.query({query: gql(queries.getDevices)});
+  res.json(queryResult.data.devices);
 });
+
+router.get('/api/devices/:imei', async function (req, res, next) {
+  console.log("querying device records");
+  const imei = req.params.imei;
+  const queryResult = await Client.query({
+    query: gql(queries.deviceRecords),
+    variables: {imei: imei}
+  })
+  const decodedTelemetry = decoder.decodeTelemetry(queryResult.data.deviceRecords);
+  res.json(decodedTelemetry);
+})
 
 module.exports = router;
